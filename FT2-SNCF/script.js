c099cbd568052ddb1defc10afafdec527a6a7362
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-01-18 15:33:10 +0100

    V2
    V2

--- FT2-SNCF/script.js
+++ FT2-SNCF/script.js
@@ -6,3 +6,11 @@ var btn = document.getElementById("btn");
 function toggleBtn(){
   btn.classList.toggle("active");
 }
+
+
+const btn2 = document.querySelector('.btn2');
+
+btn2.addEventListener('click', () => {
+    btn2.style.backgroundColor = '#6CA5FA';
+    btn2.style.transition = '.5s ease';
+});
